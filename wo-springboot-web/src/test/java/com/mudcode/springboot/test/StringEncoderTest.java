c648bffd4a84ca488f3b9dc116e9f1fe70b7cf49
yimao <yimao@anyid.me>
2024-12-25 15:00:22 +0800

    StringEncoderTest

--- wo-springboot-web/src/test/java/com/mudcode/springboot/test/StringEncoderTest.java
+++ wo-springboot-web/src/test/java/com/mudcode/springboot/test/StringEncoderTest.java
@@ -1,19 +1,26 @@
 package com.mudcode.springboot.test;
 
+import com.google.common.hash.Hashing;
 import com.mudcode.springboot.common.encoder.HexEncoder;
+import org.apache.commons.codec.digest.MurmurHash2;
+import org.apache.commons.codec.digest.MurmurHash3;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.HexFormat;
+import java.util.Random;
+import java.util.zip.CRC32;
 
-public class BigIntegerTest {
+public class StringEncoderTest {
 
     @Test
-    public void test() {
+    public void testToString() {
         String str = "Hello, World!";
         System.out.println("str length: " + str.length());
         byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
@@ -66,6 +73,31 @@ public class BigIntegerTest {
 
         DecimalFormat df = new DecimalFormat("#.####");
         System.out.println(df.format(d));
+    }
+
+    @Test
+    public void testCRC32() {
+        CRC32 crc = new CRC32();
+        crc.update(ByteBuffer.wrap("Hello, World!".getBytes(StandardCharsets.UTF_8)));
+        System.out.println(crc.getValue());
+    }
+
+    @Test
+    public void testMurmurHash() {
+        byte[] bytes = new byte[1024];
+        Random random = new Random();
+        random.nextBytes(bytes);
+
+        // MurmurHash2
+        long hash64 = MurmurHash2.hash64(bytes, bytes.length);
+        System.out.println("MurmurHash2.hash64(): " + hash64);
+
+        // MurmurHash3
+        long[] hash128 = MurmurHash3.hash128x64(bytes);
+        System.out.println("MurmurHash3.hash128x64(): " + Arrays.toString(hash128));
+
+        // google guava hashing hash128
+        System.out.println("Hashing.murmur3_128(): " + Hashing.murmur3_128().hashBytes(bytes).asLong());
     }
 
 }
